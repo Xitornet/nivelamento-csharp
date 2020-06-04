cae8463000665789b84b9fc55b5b4c8028d6da76
Victor Oliveira <hebertyxitor7@gmail.com>
2020-06-03 22:22:42 -0300

    Fiz um código mais organizado e eficiente.
    Fiz esse código para ser mais interativo e de melhor entendimento.

--- uri1010/uri1010/Program.cs
+++ uri1010/uri1010/Program.cs
@@ -1,26 +1,43 @@
-﻿using System;
+using System;
 using System.Globalization;
 
-namespace uri1010 {
-    class Program {
-        static void Main(string[] args) {
+namespace exercicio05
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] num = new double[9];
 
-            int cod1, cod2, qte1, qte2;
-            double preco1, preco2, total;
+            Console.Write("Insira o seu nome: ");
+            string nome = Console.ReadLine(); 
 
-            string[] valores = Console.ReadLine().Split(' ');
-            cod1 = int.Parse(valores[0]);
-            qte1 = int.Parse(valores[1]);
-            preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            Console.Write("\n1-) Número de peça: ");
+            num[0] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n1-) Quantidade de unidades: ");
+            num[1] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n1-) Valor da peça: ");
+            num[2] = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            valores = Console.ReadLine().Split(' ');
-            cod2 = int.Parse(valores[0]);
-            qte2 = int.Parse(valores[1]);
-            preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            num[3] = num[1] * num[2];
+            Console.Write("\n--------------------------------------------------\n");
+            Console.Write("\n2-) Número de peça: ");
+            num[4] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n2-) Quantidade de unidades: ");
+            num[5] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n2-) Valor da peça: ");
+            num[6] = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            total = preco1 * qte1 + preco2 * qte2;
+            num[7] = num[5] * num[6];
+            num[8] = num[7] + num[3];
 
-            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.Clear();
+            Console.WriteLine("Todas as informações sobre a sua compra {0}.\n",nome);
+            Console.WriteLine("1-) Informações: número da peça: {0} valor da compra das peças: {1:c}", num[0],num[3]);
+            Console.WriteLine("2-) Informações: número da peça: {0} valor da compra das peças: {1:c}", num[4], num[7]);
+
+            Console.WriteLine("\nValor total a pagar: R$:{0}",num[8].ToString("F2",CultureInfo.InvariantCulture));
+
         }
     }
 }
